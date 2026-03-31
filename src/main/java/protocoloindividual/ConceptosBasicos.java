6321ea36130049056b02fb30f0dccceb5b80eb84
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2026-03-31 16:10:37 -0500

    chore(init): Se crea la estructura base del protocolo individual en Java

---
+++ src/main/java/protocoloindividual/ConceptosBasicos.java
@@ -0,0 +1,29 @@
+package protocoloindividual;
+
+public class ConceptosBasicos {
+
+    public static void main(String[] args) {
+
+        //Declaramos y tambien iniciamos el arreglo
+        String[] nombres = {"Valentina", "Maria", "Eloina"};
+
+        //Con este ciclo se recorre y se muestran loe elementos en el arreglo 
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+
+        // Accederé a una posición dentro de el
+        System.out.println("");
+        System.out.println("El tercer nombre en el arreglo es " + nombres[2]);
+        System.out.println("");
+
+        // Ahora bien modificamos un valor dentro del arreglo
+        nombres[2] = "Alirio";
+
+        //Finalmente mostramos el arreglo modificado:
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+
+    }
+}
---
+++ src/main/java/protocoloindividual/EjemplosArreglos.java
@@ -0,0 +1,7 @@
+
+package protocoloindividual;
+
+
+public class EjemplosArreglos {
+    
+}
