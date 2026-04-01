40285b5309a8397cf20c444c0af2da0dbe55d7b0
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2026-03-31 23:36:16 -0500

    feat(matrices): implementar actividad 3x3 con recorrido, suma e intercambio de filas

--- Unidad1/ProtocoloIndividual/src/main/java/protocoloindividual/EjemplosMatrices.java
+++ Unidad1/ProtocoloIndividual/src/main/java/protocoloindividual/EjemplosMatrices.java
@@ -1,13 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package protocoloindividual;
 
-/**
- *
- * @author ASUS
- */
 public class EjemplosMatrices {
-    
+
+    public static void main(String[] args) {
+
+        //Declaramas e iniciamos la atriz 3x3
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Se imprime por consola
+        System.out.println();
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        // Recorremos la matriz
+        System.out.println();
+        System.out.println();
+        for (int j = 0; j < matriz[0].length; j++) {
+            for (int i = 0; i < matriz.length; i++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+
+        //Sumar todo
+        int suma = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                suma += matriz[i][j];
+            }
+        }
+        System.out.println("\nLa suma de todo es: " + suma);
+
+        System.out.println("");
+
+        //Intercambio de la primera fila con la última
+        int[] temporal = matriz[0];
+        matriz[0] = matriz[2];
+        matriz[2] = temporal;
+
+        //mostramos como queda:
+        System.out.println("\nLa nueva matriz es:");
+        System.out.println("");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
+    
+
