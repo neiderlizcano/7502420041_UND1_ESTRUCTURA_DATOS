1db43a4acd50b2771022a5bd87a36ea8ca7e8f97
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2026-03-31 23:01:22 -0500

    feat(matrices): crear clase ConceptosBasicosMatrices y desarrollar fundamentos básicos de arrays bidimensionales en Java

--- Unidad1/ProtocoloIndividual/src/main/java/protocoloindividual/ConceptosBasicosMatrices.java
+++ Unidad1/ProtocoloIndividual/src/main/java/protocoloindividual/ConceptosBasicosMatrices.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package protocoloindividual;
 
-/**
- *
- * @author ASUS
- */
 public class ConceptosBasicosMatrices {
-    
+
+    public static void main(String[] args) {
+        //Declaramos  e inicialización de la matriz
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        for (int j = 0; j < matriz[0].length; j++) {
+            for (int i = 0; i < matriz.length; i++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    int[][] matrizIrregular = {
+        {9, 6, 5},
+        {5, 3},
+        {8, 1, 1, 0}
+    };
+
 }
